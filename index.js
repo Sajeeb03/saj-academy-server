c001d3d82e802468f80335c402fe01237ac7441e
Sajeb03 <sajeebmuntasir0@gmail.com>
2022-10-24 22:29:09 +0600

    courese api added

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
     res.send("Learning-platform-server is running")
 })
 
+app.get("/courses", (req, res) => {
+    res.send("Hello")
+})
+
 app.listen(port, () => {
     console.log("Server is running at port:", port);
 })
