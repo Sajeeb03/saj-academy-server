d57660a4ba51d58310a3abcece61f68fbb53c918
muntasir <sajeebmuntasir0@gmail.com>
2022-12-04 16:29:22 +0600

    payment method added

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const cors = require('cors');
 const app = express();
+require("dotenv").config();
+const stripe = require("stripe")(process.env.SECRET_KEY);
+
 app.use(cors());
+app.use(express.json());
 
 const port = process.env.Port || 5000;
+
 const courses = require('./data/courses.json')
 
 app.get('/', (req, res) => {
@@ -18,6 +23,27 @@ app.get("/courses/:id", (req, res) => {
     const id = req.params.id;
     const course = courses.find(cr => cr.id === id)
     res.send(course)
+})
+
+//
+app.post("/create-payment-intent", async (req, res) => {
+    const { total } = req.body;
+    // console.log(total)
+    try {
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: total * 100,
+            currency: "usd",
+            automatic_payment_methods: {
+                enabled: true
+            }
+        })
+        res.send({
+            clientSecret: paymentIntent.client_secret
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
 })
 
 app.listen(port, () => {
